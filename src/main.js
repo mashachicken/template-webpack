411fb7a2f367519abcd2b995430e2c29447194c7
Masha Ivy <mashanechmo@gmail.com>
2019-11-15 11:10:24 -0800

    add a contructor and a prototype

---
+++ src/main.js
@@ -0,0 +1,11 @@
+class Years {
+  constructor () {
+    this.mercuryYears =  mercuryYears,
+    this.venusYears =  venusYears,
+    this.marsYears = marsYears,
+    this.jupiterYears = jupiterYears
+  }
+  mercuryCount() {
+    mercuryYears * .24
+  }
+};

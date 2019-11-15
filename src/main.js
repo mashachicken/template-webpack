9e625f09dc442367f2a258038d7340924546fe1c
Masha Ivy <mashanechmo@gmail.com>
2019-11-15 12:10:51 -0800

    refactor a code a little bit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
 class Years {
-  constructor () {
-    this.mercuryYears =  mercuryYears,
-    this.venusYears =  venusYears,
-    this.marsYears = marsYears,
-    this.jupiterYears = jupiterYears
+  constructor (userYears) {
+    this.years = userYears
+    // this.mercuryYears =  mercuryYears,
+    // this.venusYears =  venusYears,
+    // this.marsYears = marsYears,
+    // this.jupiterYears = jupiterYears
   }
   mercuryCount() {
-    mercuryYears * .24
+  let mercuryYears = (userYears * .24);
+  return mercuryYears
   }
+  venusCount() {
+  let mercuryYears = (userYears * .24);
+  return mercuryYears
+}
+  marsCount() {
+  let marsYears = (userYears * 1.88);
+  return marsYears
+}
+  jupiterCount() {
+  let jupiterYears = (userYears * 11.86);
+  return jupiterYears
+  }
+
 };

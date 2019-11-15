112b44e55178b7a09c30785ae07e1dbe8a734f74
Masha Ivy <mashanechmo@gmail.com>
2019-11-15 13:43:09 -0800

    add import statements

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
+import $ from 'jquery';
+import 'bootstrap';
+import './styles.css';
+import { Years } from './../src/calculator.js';
 $(document).ready(function){
   ("#calculateYearsForm").submit(function(event{
+    event.preventDefault()
     const userAge = ("#solar").val();
     let userYears = new Years (userAge)
     $("#mercury").html(userYears.mercuryCount());

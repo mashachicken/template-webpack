4ab6f96abf32635ddef57a1a3a5ffb2ff6814781
Masha Ivy <mashanechmo@gmail.com>
2019-11-16 20:22:07 -0800

    add another prototype that counts years left on every planet and appends it into ul, also add some ui

--- src/main.js
+++ src/main.js
@@ -5,11 +5,18 @@ $(document).ready(function(){
   $("#calculateYearsForm").submit(function(event) {
     event.preventDefault()
     const userAge = $("#solar").val();
-    let userYears = new Years (userAge)
+    const lifeExpectancy = $("#life").val();
+    console.log(lifeExpectancy)
+    let userYears = new Years (userAge, lifeExpectancy)
     userYears.mercuryCount()
     userYears.venusCount()
     userYears.marsCount()
     userYears.jupiterCount()
+    console.log(userYears.yearsLeft())
+    userYears.yearsLeft().forEach(function(year){
+    $("#yearsleft").append("<li>" + year + "</li>")
+  });
+
     $(".mercury").html(userYears.mercuryYears);
     $(".venus").html(userYears.venusYears);
     $(".mars").html(userYears.marsYears);

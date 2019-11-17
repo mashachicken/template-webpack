31d82e055f66a2867a166c9024616929bb5423d9
Masha Ivy <mashanechmo@gmail.com>
2019-11-17 01:39:09 -0800

    convert the life expectansy to planet years before counting years left to live

--- src/calculator.js
+++ src/calculator.js
@@ -1,22 +1,30 @@
 export class Years {
-  constructor (userAge, mercuryYears, venusYears, marsYears, jupiterYears) {
+  constructor (userAge, lifeExpectancy, mercuryYears, venusYears, marsYears, jupiterYears) {
     this.userAge = userAge,
+    this.lifeExpectancy = lifeExpectancy,
     this.mercuryYears = mercuryYears,
     this.venusYears = venusYears,
     this.marsYears = marsYears,
     this.jupiterYears = jupiterYears
   }
   mercuryCount() {
-  this.mercuryYears = (this.userAge * .24);
+    this.mercuryYears = (this.userAge * .24);
   }
   venusCount() {
-  this.venusYears = (this.userAge * .62);
+    this.venusYears = (this.userAge * .62);
   }
   marsCount() {
-  this.marsYears = (this.userAge * 1.88);
+    this.marsYears = (this.userAge * 1.88);
   }
   jupiterCount() {
-  this.jupiterYears = (this.userAge * 11.86);
+    this.jupiterYears = (this.userAge * 11.86);
   }
-
-  };
+  yearsLeft() {
+    let yearsLeftArray = [];
+    yearsLeftArray.push((this.lifeExpectancy * .24) - this.mercuryYears)
+    yearsLeftArray.push((this.lifeExpectancy * .62) - this.venusYears)
+    yearsLeftArray.push((this.lifeExpectancy * 1.88) - this.marsYears)
+    yearsLeftArray.push((this.lifeExpectancy * 11.86) - this.jupiterYears)
+    return yearsLeftArray
+  }
+};

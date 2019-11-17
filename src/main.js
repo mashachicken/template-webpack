ddb705d17831fd16e3153aa537ad56f05b18f7ab
Masha Ivy <mashanechmo@gmail.com>
2019-11-17 00:44:49 -0800

    count and append years surpassed

--- src/main.js
+++ src/main.js
@@ -6,20 +6,25 @@ $(document).ready(function(){
     event.preventDefault()
     const userAge = $("#solar").val();
     const lifeExpectancy = $("#life").val();
-    console.log(lifeExpectancy)
     let userYears = new Years (userAge, lifeExpectancy)
     userYears.mercuryCount()
     userYears.venusCount()
     userYears.marsCount()
     userYears.jupiterCount()
-    console.log(userYears.yearsLeft())
+    // userYears.yearsPast()
+
     userYears.yearsLeft().forEach(function(year){
-    $("#yearsleft").append("<li>" + year + "</li>")
-  });
+    if (year >= 0) {
+      $("#yearsleft").append("<li>" + year + "</li>")
+    } else {
+      $("#yearspast").append("<li>" + year * (-1) + "</li>")
+      console.log("u dead")
+    };
+  })
 
     $(".mercury").html(userYears.mercuryYears);
     $(".venus").html(userYears.venusYears);
     $(".mars").html(userYears.marsYears);
     $(".jupiter").html(userYears.jupiterYears);
-});
+  });
 });

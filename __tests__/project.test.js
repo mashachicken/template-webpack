0b8d4ea75b822f2204ed535a63558fe2cd14c6f9
Masha Ivy <mashanechmo@gmail.com>
2019-11-17 21:59:15 -0800

    refactor

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -1,8 +1,8 @@
 import { Years } from './../src/calculator.js';
 describe('Years', () => {
   test('Should correctly calculate user`s age in mercury years', () => {
-    let testAge = new Years(20);
+    let testAge = new Years(30);
     testAge.mercuryCount()
-    expect(testAge.mercuryYears).toEqual("50");
+    expect(testAge.mercuryYears).toEqual(7.2);
   });
 });

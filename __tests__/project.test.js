e3d3f5719ea5fbb19cc3767c9954eb593df94c1d
Masha Ivy <mashanechmo@gmail.com>
2019-11-17 21:51:43 -0800

    test failed

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -1,8 +1,8 @@
 import { Years } from './../src/calculator.js';
 describe('Years', () => {
   test('Should correctly calculate user`s age in mercury years', () => {
-    var testAge = new Years(20);
-    userYears.mercuryCount()
-    expect(testAge.mercuryYears).toEqual(50);
+    let testAge = new Years(20);
+    testAge.mercuryCount()
+    expect(testAge.mercuryYears).toEqual("50");
   });
 });

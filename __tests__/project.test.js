6c37ea67ef850af6329e6e755db2d0846d3c1624
Masha Ivy <mashanechmo@gmail.com>
2019-11-17 22:13:12 -0800

    write a 3rd test that should fail

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -5,7 +5,12 @@ describe('Years', () => {
     testAge.mercuryCount()
     expect(testAge.mercuryYears).toEqual(7.199999999999999);
   });
-  let testAge = new Years(30);
-  testAge.venusCount()
-  expect(testAge.venusYears).toEqual(18.6);
+  test('Should correctly calculate user`s age in mercury years', () => {
+    let testAge = new Years(30);
+    testAge.venusCount()
+    expect(testAge.venusYears).toEqual(18.6);
+  });
+  let testAge = new Years(20);
+  testAge.marsCount()
+  expect(testAge.marsYears).toEqual(17.599999999999994);
 });

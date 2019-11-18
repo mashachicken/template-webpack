dbc871ae82d83f0d33815c2385f87db472fd7646
Masha Ivy <mashanechmo@gmail.com>
2019-11-17 22:18:40 -0800

    write a 4th spec

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -5,12 +5,15 @@ describe('Years', () => {
     testAge.mercuryCount()
     expect(testAge.mercuryYears).toEqual(7.199999999999999);
   });
-  test('Should correctly calculate user`s age in mercury years', () => {
+  test('Should correctly calculate user`s age in venus years', () => {
     let testAge = new Years(30);
     testAge.venusCount()
     expect(testAge.venusYears).toEqual(18.6);
   });
-  let testAge = new Years(20);
-  testAge.marsCount()
-  expect(testAge.marsYears).toEqual(37.599999999999994);
-});
+  test('Should correctly calculate user`s age in  mars years', () => {
+    let testAge = new Years(20);
+    testAge.marsCount()
+    expect(testAge.marsYears).toEqual(37.599999999999994);
+  });
+  test('Should correctly calculate user`s age in jupiter years', () => {
+  }

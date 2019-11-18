8c53c6796057eeb209bcf22fccda3deb3cbe9223
Masha Ivy <mashanechmo@gmail.com>
2019-11-17 22:19:24 -0800

    write a test that will fail

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -16,4 +16,7 @@ describe('Years', () => {
     expect(testAge.marsYears).toEqual(37.599999999999994);
   });
   test('Should correctly calculate user`s age in jupiter years', () => {
-  }
+    let testAge = new Years(20);
+    testAge.jupiterCount()
+    expect(testAge.jupiterYears).toEqual(994);
+  });

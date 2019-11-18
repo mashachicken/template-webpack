ea6ccf5e3ff9112c1ba2b65a207e47b339aaee57
Masha Ivy <mashanechmo@gmail.com>
2019-11-17 21:37:50 -0800

    write a test that is expected to fail

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -1,5 +1,8 @@
 import { Years } from './../src/calculator.js';
-describe('PizzaPie', () => {
+describe('Years', () => {
   test('Should correctly calculate user`s age in mercury years', () => {
-  })
-})
+    var testAge = new Years(20);
+    userYears.mercuryCount()
+    expect(testAge.mercuryYears).toEqual(50);
+  });
+});
